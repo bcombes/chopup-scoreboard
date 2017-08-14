da4da268f38152c34425d2039984fda1471edd31
bcombes <bayo@chopup.me>
2017-08-14 16:00:57 +0100

    missed a spot

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 let scoreboards = {};
 
 scoreboards.scoreboard = require('./lib/scoreboard');
-scoreboards.scoreboard2 = require('./lib/scoreboard2')
-
+scoreboards.scoreboard2 = require('./lib/scoreboard2');
+scoreboards.scoreboard3 = require('./lib/scoreboard3');
 
 module.exports = scoreboards;
 
